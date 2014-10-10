13b165f59a4c85cdab9388b1bbcd1958d4b0d25a
chenfushan <chenfushan1992@gmail.com>
2014-10-10 19:01:59 +0800

    done the candy algrithm

--- candy_leetcode/candy_leetcode/main.cpp
+++ candy_leetcode/candy_leetcode/main.cpp
@@ -15,8 +15,51 @@ public:
     int candy(vector<int> &ratings){
         int candy_num = 0;
         vector<int>::iterator iter;
-        
-        return 0;
+        iter = ratings.begin();
+        vector<int>::reverse_iterator riter;
+        riter = ratings.rbegin();
+        unsigned long int length = ratings.size();
+        if (length == 0) {
+            return 0;
+        }
+        int a[length],b[length];
+        a[0] = 1;
+        int i = 1;
+        int prerat = *ratings.begin();
+        for (++iter; iter != ratings.end(); iter++,i++) {
+            if (*iter > prerat) {
+                a[i] = a[i-1]+1;
+            }
+            if (*iter == prerat) {
+                a[i] = 1;
+            }
+            if (*iter < prerat) {
+                a[i] = 1;
+            }
+            prerat = *iter;
+        }
+        b[length-1] = 1;
+        i = (int)length-2;
+        prerat = *riter;
+        for (++riter; riter != ratings.rend(); riter++,i--) {
+            if (*riter > prerat) {
+                b[i] = b[i+1]+1;
+            }
+            if (*riter == prerat) {
+                b[i] = 1;
+            }
+            if (*riter < prerat) {
+                b[i] = 1;
+            }
+            prerat = *riter;
+        }
+        for (i = 0; i < length; i++) {
+            candy_num += Max(a[i],b[i]);
+        }
+        return candy_num;
+    }
+    int Max(int a, int b){
+        return a>b?a:b;
     }
 };
 
